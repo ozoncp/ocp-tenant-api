0e811454b7086b502a6432ff02e5a897742f7bed
a.krivolutskiy <andrey.krivolutskiy@stoloto.ru>
2021-05-25 18:24:35 +0300

    Add package utils

--- cmd/ozon-tenant-api/main.go
+++ cmd/ozon-tenant-api/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ozoncp/ocp-tenant-api/internal/utils"
+)
 
 func main() {
 	fmt.Println("Hi! This cmd will be done by Krivolutskiy Andrey.")
+
+	// делим массив
+	a := make([]int64, 88)
+	for index, _ := range a {
+		a[index] = int64(index)
+		fmt.Printf(" %v", index)
+	}
+	fmt.Println()
+	fmt.Println()
+	b := utils.Split(a, 10)
+	for _, value := range b {
+		for _, value2 := range value {
+			fmt.Printf(" %v", value2)
+		}
+		fmt.Println()
+	}
+	// выворачиваем мапу
+	baseMap := map[int64]int64{}
+	baseMap[0] = 100
+	baseMap[1] = 200
+	baseMap[2] = 300
+	swapResult := utils.SwapKeyValue(baseMap)
+	for key, value := range swapResult {
+		fmt.Printf("%v %v \n", key, value)
+	}
+	// удаляем первые 10 елементов
+	removeResult := utils.RemoveElements(a, b[0])
+	for _, value := range removeResult {
+		fmt.Printf(" %v", value)
+	}
 }
---
+++ internal/utils/utils.go
@@ -0,0 +1,45 @@
+package utils
+
+// Разделение на слайса на батчи - исходный слайс конвертировать в слайс слайсов - чанки одинкового размера (кроме последнего)
+func Split(toSplit []int64, size int) [][]int64 {
+	sizeOfBatches := len(toSplit) / size
+	if len(toSplit)%size != 0 {
+		sizeOfBatches += 1
+	}
+	batches := make([][]int64, sizeOfBatches, sizeOfBatches)
+	index := 0
+	for index+size < len(toSplit) {
+		batches[index/size] = toSplit[index : index+size]
+		index += size
+	}
+	batches[sizeOfBatches-1] = toSplit[index : index+len(toSplit)%size]
+	return batches
+}
+
+// Обратный ключ - происходит конвертация отображения ("ключ-значение") в отображение ("значение-ключ")
+func SwapKeyValue(baseMap map[int64]int64) map[int64]int64 {
+	result := make(map[int64]int64, len(baseMap))
+	for key, value := range baseMap {
+		result[value] = key
+	}
+	return result
+}
+
+// Фильтрация по захордкоженному списку - нужно отфильтровать входной слайс по критерию отсутствия элемента в захардкоженном списке
+// хардкодить не хочу - будет просто два слайса. Из одного выкидываем елементы второго
+func RemoveElements(base []int64, toRemove []int64) []int64 {
+	result := make([]int64, 0, len(base))
+	for _, value := range base {
+		elementFound := false
+		for _, value2 := range toRemove {
+			if value2 == value {
+				elementFound = true
+				break
+			}
+		}
+		if !elementFound {
+			result = append(result, value)
+		}
+	}
+	return result
+}
