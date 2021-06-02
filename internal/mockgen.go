2bc9c7ae7bfe6329865af3f470105bc10dbe8639
a.krivolutskiy <andrey.krivolutskiy@stoloto.ru>
2021-06-03 01:47:26 +0300

    fix mocks

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -1,4 +1,4 @@
 package internal
 
 //go:generate mockgen -destination=./mocks/flusher_mock.go -package=mocks github.com/ozoncp/ocp-tenant-api/internal/flusher Flusher
-//go:generate mockgen -destination=./mocks/flusher_mock.go -package=mocks github.com/ozoncp/ocp-tenant-api/internal/repo Repo
+//go:generate mockgen -destination=./mocks/repo_mock.go -package=mocks github.com/ozoncp/ocp-tenant-api/internal/repo Repo
