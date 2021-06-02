1caa4d0ea08ce75be97e578cf1195b1b745e800d
a.krivolutskiy <andrey.krivolutskiy@stoloto.ru>
2021-06-03 02:21:49 +0300

    add flusher test

--- internal/tenant/tenant.go
+++ internal/tenant/tenant.go
@@ -15,3 +15,11 @@ func (t Tenant) String() string {
 		"; Name: " + t.Name +
 		"; Type: " + strconv.FormatUint(uint64(t.Type), 10)
 }
+
+func New(id uint64, name string, tenantType uint8) Tenant {
+	return Tenant{
+		Id:   id,
+		Name: name,
+		Type: tenantType,
+	}
+}
