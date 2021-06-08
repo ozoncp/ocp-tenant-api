d5cef87ac01e87ce2bb3762d55bb94c868c85691
a.krivolutskiy <andrey.krivolutskiy@stoloto.ru>
2021-06-08 18:32:32 +0300

    add saverV1

---
+++ internal/saver/saver.go
@@ -0,0 +1,92 @@
+package saver
+
+import (
+	"github.com/ozoncp/ocp-tenant-api/internal/flusher"
+	"github.com/ozoncp/ocp-tenant-api/internal/tenant"
+	"time"
+)
+
+type Saver interface {
+	Save(entity tenant.Tenant)
+	Close()
+}
+
+type RemoveRule uint8
+
+const (
+	All RemoveRule = iota
+	Last
+)
+
+type saver struct {
+	capacity   uint
+	flusher    flusher.Flusher
+	removeRule RemoveRule
+	buffer     []tenant.Tenant
+	toSave     chan tenant.Tenant
+	toClose    chan struct{}
+	close      bool
+	ticker     *time.Ticker
+}
+
+func (s saver) Work() {
+	for {
+		select {
+		case entity := <-s.toSave:
+			if len(s.buffer) == cap(s.buffer) {
+				switch s.removeRule {
+				case All:
+					s.buffer = s.buffer[:0]
+				case Last:
+					s.buffer = s.buffer[:len(s.buffer)-1]
+				}
+			}
+			s.buffer = append(s.buffer, entity)
+		case <-s.toClose:
+			s.Flush()
+			break
+		case <-s.ticker.C:
+			s.Flush()
+		}
+	}
+}
+
+func (s saver) Flush() {
+	if len(s.buffer) > 0 {
+		flushResult := s.flusher.Flush(s.buffer)
+		if flushResult == nil {
+			s.buffer = s.buffer[:0]
+		} else {
+			s.buffer = s.buffer[:copy(s.buffer, flushResult)]
+		}
+	}
+}
+
+func (s saver) Save(entity tenant.Tenant) {
+	if s.close {
+		panic("Saver closed.")
+	}
+	s.toSave <- entity
+}
+
+func (s saver) Close() {
+	s.ticker.Stop()
+	s.toClose <- struct{}{}
+	s.close = true
+}
+
+// NewSaver возвращает Saver с поддержкой переодического сохранения
+func NewSaver(
+	capacity uint,
+	flusher flusher.Flusher,
+	removeRule RemoveRule,
+	period time.Duration) Saver {
+	result := saver{
+		capacity:   capacity,
+		flusher:    flusher,
+		removeRule: removeRule,
+	}
+	result.ticker = time.NewTicker(period)
+	go result.Work()
+	return result
+}
